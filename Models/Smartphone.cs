e564f6c9ac4f556bb2af704505ee934dac08d7dc
RenathaLoiza <renatha.loiza@hotmail.com>
2024-01-14 10:43:46 -0300

    Desafio Completo

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -1,14 +1,22 @@
 namespace DesafioPOO.Models
 {
     public abstract class Smartphone
-    {
+    {   
+        //foi propriedades faltantes de acordo com o diagrama
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        protected string Modelo { get; set; }
+        protected string IMEI { get; set; }
+        protected int Memoria{ get; set; }
+        
+        
+        // foi adicionados os parâmetros do construtor para as propriedades
+        public Smartphone(string numero,string modelo,string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            
         }
 
         public void Ligar()
